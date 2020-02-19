424a66094b657f548d9e80df063d1d89a2eaaf8f
Jing <boundino@gmail.com>
2020-02-19 20:48:29 +0100

    keep D0 channel open only, add 2017 ZB data input file

--- test/DnBfinder_to_Forest_94X.sh
+++ test/DnBfinder_to_Forest_94X.sh
@@ -5,7 +5,8 @@ FORESTS=(runForestAOD_pp_DATA_94X runForestAOD_pp_MC_94X)
 RUNONMC=(False True)
 # DIFFPATH=("process.hltanalysisReco *" "process.hltanalysis * process.runAnalyzer *")
 INFILES=(
-    "file:/afs/cern.ch/work/w/wangj/public/DoubleMuon/Run2017G-17Nov2017-v1/DEBD2F77-0E38-E811-8161-A4BF0108B83A.root"
+    # "file:/afs/cern.ch/work/w/wangj/public/DoubleMuon/Run2017G-17Nov2017-v1/DEBD2F77-0E38-E811-8161-A4BF0108B83A.root"
+    "file:/afs/cern.ch/work/w/wangj/public/HIZeroBias2/Run2017G-17Nov2017-v1/141428A3-8F24-E811-B2A0-1CC1DE056008.root"
     "file:/afs/cern.ch/work/w/wangj/public/BJPsiMM_TuneCUETP8M1_5p02TeV_pythia8/RunIIpp5Spring18DR-94X_mc2017_realistic_forppRef5TeV-v2/C00FF4F0-D421-E911-9F69-141877410E71.root"
 )
 
@@ -30,14 +31,14 @@ finderMaker_75X(process, AddCaloMuon, runOnMC, HIFormat, UseGenPlusSim, VtxLabel
 process.Dfinder.MVAMapLabel = cms.InputTag(TrkLabel,"MVAValues")
 process.Dfinder.makeDntuple = cms.bool(True)
 process.Dfinder.tkPtCut = cms.double(0.5) # before fit
-process.Dfinder.dPtCut = cms.vdouble(2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0) # before fit
+process.Dfinder.dPtCut = cms.vdouble(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0) # before fit
 process.Dfinder.VtxChiProbCut = cms.vdouble(0.05, 0.05, 0.0, 0.0, 0.0, 0.0, 0.05, 0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.05, 0.05)
 process.Dfinder.dCutSeparating_PtVal = cms.vdouble(5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5.)
 process.Dfinder.tktkRes_svpvDistanceCut_lowptD = cms.vdouble(0., 0., 0., 0., 0., 0., 0., 0., 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0.)
 process.Dfinder.tktkRes_svpvDistanceCut_highptD = cms.vdouble(0., 0., 0., 0., 0., 0., 0., 0., 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0.)
 process.Dfinder.svpvDistanceCut_lowptD = cms.vdouble(2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0., 0., 0., 0., 0., 2.5, 2.5)
 process.Dfinder.svpvDistanceCut_highptD = cms.vdouble(2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0., 0., 0., 0., 0., 2.5, 2.5)
-process.Dfinder.Dchannel = cms.vint32(1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1)
+process.Dfinder.Dchannel = cms.vint32(1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 
 process.dfinder = cms.Path(process.DfinderSequence)
 
