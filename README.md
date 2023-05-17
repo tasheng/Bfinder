2fed689804358c4e862ddc0f39181c3f34f1085b
boundino <boundino@gmail.com>
2023-05-17 16:30:59 +0200

    move to 125X for all miniAOD

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 To setup Bfinder
 =====
 
-Ref: https://twiki.cern.ch/twiki/bin/view/CMS/L1HITaskForce2022#Instructions_to_run_the_L1Em_AN1
+2022 or 2018 PbPb data/MC from miniAOD
+Ref: https://twiki.cern.ch/twiki/bin/viewauth/CMS/HiForestSetup#Setup_for_12_5_X_2022_or_2018_Pb
 
 ```
-cmsrel CMSSW_12_4_0
-cd CMSSW_12_4_0/src
+cmsrel CMSSW_12_5_0
+cd CMSSW_12_5_0/src
 cmsenv
-git cms-merge-topic CmsHI:forest_CMSSW_12_4_0
+git cms-merge-topic CmsHI:forest_CMSSW_12_5_0
 scram b -j4
 ```
 
@@ -18,7 +19,7 @@ To add D/Bfinder to forest:
 cd $CMSSW_BASE/src
 cmsenv
 git clone -b Dfinder_12XX_miniAOD https://github.com/boundino/Bfinder.git --depth 1
-source Bfinder/test/DnBfinder_to_Forest_124X_miniAOD.sh
+source Bfinder/test/DnBfinder_to_Forest_125X_miniAOD.sh
 scram b -j4
 mkdir -p dfinder && cp HeavyIonsAnalysis/Configuration/test/forest_miniAOD_run3_MC_wDfinder.py dfinder/
 cd dfinder/
@@ -28,5 +29,5 @@ To run:
 =====
 
 ```
-cmsRun forest_miniAOD_run3_MC_wDfinder.py
+cmsRun forest_miniAOD_run2_DATA_wDfinder.py
 ```
---
+++ test/DnBfinder_to_Forest_125X_miniAOD.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+PATHTOTEST=$CMSSW_BASE/src/HeavyIonsAnalysis/Configuration/test/
+FORESTS=(forest_miniAOD_run2_DATA forest_miniAOD_run2_MC forest_miniAOD_run3_DATA forest_miniAOD_run3_MC)
+RUNONMC=(False True False True)
+INFILES=(
+    "/store/hidata/HIRun2018A/HIHeavyFlavor/MINIAOD/PbPb18_MiniAODv1-v1/240000/fdf05837-6240-4773-8cd7-d889d991ba17.root"
+    "file:/afs/cern.ch/work/w/wangj/public/Pythia8_DzeroToKPi_prompt_Pthat15_TuneCP5_5020GeV_Drum5F/step3_reco.root"
+    "file:/afs/cern.ch/work/w/wangj/public/Pythia8_DzeroToKPi_prompt_Pthat15_TuneCP5_5020GeV_Drum5F/step3_reco.root"
+    "file:/afs/cern.ch/work/w/wangj/public/Pythia8_DzeroToKPi_prompt_Pthat15_TuneCP5_5020GeV_Drum5F/step3_reco.root"
+)
+MINIMUMTREES=0
+
+cc=0
+for FOREST in ${FORESTS[@]}
+do
+    ##
+    cp ${PATHTOTEST}/${FOREST}.py ${PATHTOTEST}/${FOREST}_wDfinder.py
+
+    echo '
+#################### D/B finder ################# 
+AddCaloMuon = False 
+runOnMC = '${RUNONMC[cc]}' ## !!
+HIFormat = False 
+UseGenPlusSim = False 
+# VtxLabel = "unpackedTracksAndVertices"
+VtxLabel = "offlineSlimmedPrimaryVerticesRecovery"
+TrkLabel = "packedPFCandidates"
+GenLabel = "prunedGenParticles"
+TrkChi2Label = "packedPFCandidateTrackChi2"
+useL1Stage2 = True
+HLTProName = "HLT"
+from Bfinder.finderMaker.finderMaker_75X_cff import finderMaker_75X 
+finderMaker_75X(process, AddCaloMuon, runOnMC, HIFormat, UseGenPlusSim, VtxLabel, TrkLabel, TrkChi2Label, GenLabel, useL1Stage2, HLTProName)
+process.Dfinder.MVAMapLabel = cms.InputTag(TrkLabel, "MVAValues")
+process.Dfinder.makeDntuple = cms.bool(True)
+process.Dfinder.tkPtCut = cms.double(2.0) # before fit
+process.Dfinder.tkEtaCut = cms.double(2.4) # before fit
+process.Dfinder.dPtCut = cms.vdouble(10.0, 10.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0) # before fit
+process.Dfinder.VtxChiProbCut = cms.vdouble(0.05, 0.05, 0.0, 0.0, 0.0, 0.0, 0.05, 0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.05, 0.05)
+process.Dfinder.dCutSeparating_PtVal = cms.vdouble(5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5.)
+process.Dfinder.tktkRes_svpvDistanceCut_lowptD = cms.vdouble(0., 0., 0., 0., 0., 0., 0., 0., 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0.)
+process.Dfinder.tktkRes_svpvDistanceCut_highptD = cms.vdouble(0., 0., 0., 0., 0., 0., 0., 0., 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0.)
+process.Dfinder.svpvDistanceCut_lowptD = cms.vdouble(2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0., 0., 0., 0., 0., 2.5, 2.5)
+process.Dfinder.svpvDistanceCut_highptD = cms.vdouble(2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0., 0., 0., 0., 0., 2.5, 2.5)
+process.Dfinder.Dchannel = cms.vint32(1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+process.Dfinder.dropUnusedTracks = cms.bool(True)
+process.Dfinder.detailMode = cms.bool(False)
+
+process.dfinder = cms.Path(process.DfinderSequence)
+
+' >> ${PATHTOTEST}/${FOREST}_wDfinder.py
+
+    ##
+    cp ${PATHTOTEST}/${FOREST}.py ${PATHTOTEST}/${FOREST}_wBfinder.py
+
+    echo '
+#################### D/B finder #################
+AddCaloMuon = False
+runOnMC = '${RUNONMC[cc]}' ## !!
+HIFormat = False
+UseGenPlusSim = False
+# VtxLabel = "unpackedTracksAndVertices"
+VtxLabel = "offlineSlimmedPrimaryVerticesRecovery"
+TrkLabel = "packedPFCandidates"
+GenLabel = "prunedGenParticles"
+useL1Stage2 = True
+HLTProName = "HLT"
+from Bfinder.finderMaker.finderMaker_75X_cff import finderMaker_75X
+finderMaker_75X(process, AddCaloMuon, runOnMC, HIFormat, UseGenPlusSim, VtxLabel, TrkLabel, GenLabel, useL1Stage2, HLTProName)
+
+process.Bfinder.MVAMapLabel = cms.InputTag(TrkLabel,"MVAValues")
+process.Bfinder.makeBntuple = cms.bool(True)
+process.Bfinder.tkPtCut = cms.double(0.8) # before fit
+process.Bfinder.tkEtaCut = cms.double(2.4) # before fit
+process.Bfinder.jpsiPtCut = cms.double(0.0) # before fit
+process.Bfinder.bPtCut = cms.vdouble(3.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0) # before fit
+process.Bfinder.Bchannel = cms.vint32(1, 0, 0, 1, 1, 1, 1)
+process.Bfinder.VtxChiProbCut = cms.vdouble(0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.10)
+process.Bfinder.svpvDistanceCut = cms.vdouble(2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 0.0)
+process.Bfinder.doTkPreCut = cms.bool(True)
+process.Bfinder.doMuPreCut = cms.bool(True)
+process.Bfinder.MuonTriggerMatchingPath = cms.vstring(
+    "HLT_HIL3Mu0NHitQ10_L2Mu0_MAXdR3p5_M1to5_v1")
+process.Bfinder.MuonTriggerMatchingFilter = cms.vstring(
+    "hltL3f0L3Mu0L2Mu0DR3p5FilteredNHitQ10M1to5")
+process.p = cms.Path(process.BfinderSequence)
+
+' >> ${PATHTOTEST}/${FOREST}_wBfinder.py
+
+    configfiles="${PATHTOTEST}/${FOREST}_wDfinder.py ${PATHTOTEST}/${FOREST}_wBfinder.py"
+    #
+    [[ $MINIMUMTREES -eq 1 ]] && {
+        cp ${PATHTOTEST}/${FOREST}_wDfinder.py ${PATHTOTEST}/${FOREST}_onlyDfinder.py
+        cp ${PATHTOTEST}/${FOREST}_wBfinder.py ${PATHTOTEST}/${FOREST}_onlyBfinder.py
+
+        for ifile in ${PATHTOTEST}/${FOREST}_onlyBfinder.py ${PATHTOTEST}/${FOREST}_onlyDfinder.py
+        do
+            sed -i "/D\/B finder/i \\
+process.ana_step = cms.Path( \\
+    process.HiForest \\
+    + \\
+    process.particleFlowAnalyser \\
+    ) \\
+" $ifile
+        done
+        configfiles=$configfiles" ${PATHTOTEST}/${FOREST}_onlyDfinder.py ${PATHTOTEST}/${FOREST}_onlyBfinder.py"
+    }
+
+    #
+    for ifile in $configfiles
+    do
+        echo '
+###############################
+import FWCore.ParameterSet.VarParsing as VarParsing
+ivars = VarParsing.VarParsing('"'"'analysis'"'"')
+
+ivars.maxEvents = -1
+ivars.outputFile='"'"'HiForestMINIAOD.root'"'"'
+ivars.inputFiles='"'${INFILES[cc]}'"'
+ivars.parseArguments() # get and parse the command line arguments
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(ivars.inputFiles),
+    # eventsToProcess = cms.untracked.VEventRange('"'"'1:236:29748033-1:236:29748033'"'"')
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(ivars.maxEvents)
+)
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string(ivars.outputFile))
+' >> $ifile
+    done
+    ##
+    cc=$((cc+1))
+done
